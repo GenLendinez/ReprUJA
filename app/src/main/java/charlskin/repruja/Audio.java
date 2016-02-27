d6544b62bad091c10f56b469d9835ff57382def3
Juan Lendinez Sanchez <JLendinez@outlook.es>
2016-02-27 19:26:19 +0100

    Initial commit

---
+++ app/src/main/java/charlskin/repruja/Audio.java
@@ -0,0 +1,56 @@
+package charlskin.repruja;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Audio extends AppCompatActivity {
+
+    private ArrayList<Cancion> listaCanciones;
+    private ArrayAdapter<Cancion> adaptador;
+    private ListView vistaCanciones;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+        vistaCanciones=(ListView)findViewById(R.id.listaCanciones);
+        listaCanciones=new ArrayList<Cancion>();
+        getListaCanciones();
+        Collections.sort(listaCanciones, new Comparator<Cancion>() {
+            public int compare(Cancion a, Cancion b) {
+                return a.getTitulo().compareTo(b.getTitulo());
+            }
+        });
+        adaptador=new ArrayAdapter<Cancion>(this,R.layout.activity_audio,listaCanciones);
+        vistaCanciones.setAdapter(adaptador);
+    }
+    public void getListaCanciones() {
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                listaCanciones.add(new Cancion(thisTitle, thisArtist,thisId));
+            }
+            while (musicCursor.moveToNext());
+        }
+
+    }
+}
---
+++ app/src/main/java/charlskin/repruja/Cancion.java
@@ -0,0 +1,24 @@
+package charlskin.repruja;
+
+/**
+ * Created by Aniki on 27/02/2016.
+ */
+public class Cancion {
+    private String titulo;
+    private String artista;
+    private long ID;
+    public Cancion(String titu,String arti, long IDS){
+        titulo=titu;
+        artista=arti;
+        ID=IDS;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+    public String getArtista(){
+        return artista;
+    }
+    public long getID(){
+        return ID;
+    }
+}
